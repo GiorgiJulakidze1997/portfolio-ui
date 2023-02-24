5c5fab119767fcb8e632aa203655deb23ac7af07
GiorgiJulakidze1997 <g.julakidze1997@gmail.com>
2023-02-24 14:35:07 +0400

    add button x

--- index.js
+++ index.js
@@ -9,4 +9,18 @@ btn.addEventListener('click', () => {
     // 👇️ show menu
     const desktop = document.querySelector(".primary-menu");
     desktop.style.display = "block";
+
+
 });
+
+const xClose = document.querySelector(".x-button-box");
+xClose.addEventListener('click', () => {
+    // hide menu
+    const desktop = document.querySelector(".primary-menu");
+    desktop.style.display = "none";
+
+    // show button
+    const mobile = document.querySelector(".mobile-primary-menu");
+    mobile.style.display = "block";
+
+})
